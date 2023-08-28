b38f4a89c452e1c5366b67d32f21c3ac037541ca
Adam Griswold <adam.griswold@nucor.com>
2023-08-28 17:02:57 +0000

    ✨(app): add borna font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,38 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import localFont from 'next/font/local'
 
 import { api } from "~/lib/api";
 
 import "~/styles/globals.css";
+
+const borna = localFont({
+  src: [
+    {
+      path: '../../public/fonts/borna-medium-webfont.woff2',
+      weight: "700"
+    },
+  ],
+})
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <style jsx global> {`
+        :root {
+          --borna-font: ${borna.style.fontFamily};
+        }
+        
+        `}</style>
+
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
