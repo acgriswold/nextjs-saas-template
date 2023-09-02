484fe68fe6677abc3b7858564286625f5aa16f15
Adam Griswold <adam.griswold@nucor.com>
2023-09-02 20:10:38 +0000

    refactor(protected-element): Catch errors

--- src/components/auth/protected-element.tsx
+++ src/components/auth/protected-element.tsx
@@ -7,7 +7,7 @@ import type { auth } from "~/types/global";
 
 export function ProtectedElement({ auth, router, children }: PropsWithChildren<{ auth: auth.PageAuth | undefined | null, router: Router }>): ReactNode {
   const { data: sessionData, status } = useSession();
-  
+
   if (!auth)
     return children;
 
@@ -19,8 +19,11 @@ export function ProtectedElement({ auth, router, children }: PropsWithChildren<{
 
   if (auth.unauthorized_redirect)
     router.push(`/api/auth/signin?msg=${auth.unauthorized_redirect.message}&callbackUrl=${auth.unauthorized_redirect.callbackUrl ?? "/"}`)
+      .catch(e => console.error(e))
   else
     router.push(`/api/auth/signin`)
+      .catch(e => console.error(e))
+
 
   return <></>
 }
